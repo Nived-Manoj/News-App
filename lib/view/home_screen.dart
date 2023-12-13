c368102579083ef783986a27a51dec5331d6ac8f
Nived-Manoj <nivedmanoj001@gmail.com>
2023-12-13 10:33:41 +0530

    initial committ

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -5,6 +5,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.amber, borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 280),
+            child: Text(
+              "Author d cruzy",
+              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            ),
+          ),
+          ListTile(
+            minVerticalPadding: 10,
+            title: Text(
+              "TTTTTTTTTTTTTTTTTTTTTTTTTTFFFFFFFFFFFFFFFFFSSSSSSSSSSSSSSSSSKKKKKKKKKKKKKKKKL",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              "dgfhghsgfhgsafghsgfsdghgagjhshdddddddddddd,,,,,,JHJJJJJJJJJJJJJJJJJJJJJJJSDAAAAAAAAAAAAAAsddshadshdjgshghsjgjhsafjgsdafgshdfgsdhfghsfhajghdgfhdgshgAAAAAAHLGhggergfelfgdlfdshfjhgfahgdshjgfh",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
